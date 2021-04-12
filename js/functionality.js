f99da2f20a38ff020a1efc81f0cabbd87dbb3918
Gokce <gokmengokces@gmail.com>
2021-04-11 18:27:27 -0700

    defer and variables added

--- js/functionality.js
+++ js/functionality.js
@@ -1,37 +1,59 @@
 
-function check_web_storage_support() {
-    if(typeof(Storage) !== "undefined") {
-        return(true);
-    }
-    else {
-        alert("Web storage unsupported!");
-        return(false);
-    }
-}
-function display_saved_note() {
-    if(check_web_storage_support() == true) {
-        result = localStorage.getItem('note');
-    }
-    if(result === null) {
-        result = "No note saved";
-    }
-    document.getElementById('area').value = result;
-}
-function save() {
-    if(check_web_storage_support() == true) {
-        var area = document.getElementById("area");
-        if(area.value != '') {
-            localStorage.setItem("note", area.value);
-        }
-        else {
-            alert("Nothing to save");
-        }
-    }
-}
+let newnote = document.querySelector("#newnote");
+let save = document.querySelector("#save");
+let cancel = document.querySelector("#newnote");
+let darktheme = document.querySelector("#newnote");
 
-function cancel() {
-    document.getElementById('area').value = "";
-}
+newnote.addEventListener("click", pink);
+save.addEventListener("click", blue);
+
+function pink(e){
+    document.body.style.backgroundColor = "pink";
+  console.log(e); // pink
+};
+
+function blue(e){
+    document.body.style.backgroundColor = "blue";
+  console.log(e); // blue
+};
+
+
+
+
+
+// function check_web_storage_support() {
+//     if(typeof(Storage) !== "undefined") {
+//         return(true);
+//     }
+//     else {
+//         alert("Web storage unsupported!");
+//         return(false);
+//     }
+// }
+// function display_saved_note() {
+//     if(check_web_storage_support() == true) {
+//         result = localStorage.getItem('note');
+//     }
+//     if(result === null) {
+//         result = "No note saved";
+//     }
+//     document.getElementById('area').value = result;
+// }
+// function save() {
+//     if(check_web_storage_support() == true) {
+//         var area = document.getElementById("area");
+//         if(area.value != '') {
+//             localStorage.setItem("note", area.value);
+//         }
+//         else {
+//             alert("Nothing to save");
+//         }
+//     }
+// }
+
+// function cancel() {
+//     document.getElementById('area').value = "";
+// }
 
 // function darktheme(){
 //     if darkthemeButton.click() ==true:
